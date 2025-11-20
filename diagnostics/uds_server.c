acbe3e1551b25f10d03a86e66d9ca9a57d33c794
Raul Latorre <raul.latorre@gmail.com>
2025-11-20 21:07:05 +0100

    Update uds_server.c

--- diagnostics/uds_server.c
+++ diagnostics/uds_server.c
@@ -3,8 +3,11 @@
 #include "dtc_manager.h"
 #include "../network/can/can_transport.h"
 #include "transport/isotp_can.h"
+#include <zephyr/kernel.h>
 
-/* Dispatch table for all UDS services */
+#define UDS_MAX_LEN 4096
+
+typedef void (*uds_service_handler_t)(uint8_t *req, uint16_t len, uint8_t *rsp, uint16_t *rsp_len);
 static uds_service_handler_t service_table[0x100];
 
 void uds_init(void)
@@ -20,10 +23,14 @@ void uds_process(void)
 {
     uint8_t req_buf[UDS_MAX_LEN];
     uint8_t rsp_buf[UDS_MAX_LEN];
+    uint16_t rsp_len = 0;
 
     int len = isotp_receive(req_buf, sizeof(req_buf));
     if (len <= 0) return;
 
-    uds_handle_request(req_buf, len, rsp_buf);
-    isotp_send(rsp_buf, rsp_buf[1] + 2);
+    uint8_t sid = req_buf[0];
+    if (service_table[sid]) {
+        service_table[sid](req_buf, len, rsp_buf, &rsp_len);
+        isotp_send(rsp_buf, rsp_len);
+    }
 }
