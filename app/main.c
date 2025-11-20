bf90cef61ff034b27c9cf6d83e36e1cabdf9127f
Raul Latorre <raul.latorre@gmail.com>
2025-11-20 20:59:04 +0100

    Update main.c

--- app/main.c
+++ app/main.c
@@ -1,20 +1,24 @@
 #include <zephyr/kernel.h>
 #include "ecu_state_machine.h"
-#include "watchdog_supervisor.h"
 #include "sensor_sim.h"
 #include "fault_injection.h"
+#include "watchdog_supervisor.h"
 #include "../diagnostics/uds_server.h"
 
 void main(void)
 {
+    printk("Starting Zephyr ECU Prototype\n");
+
     ecu_init_state_machine();
     sensor_sim_init();
+    fault_injection_init();
     watchdog_init();
     uds_init();
 
     while (1) {
         ecu_state_update();
         sensor_sim_update();
+        fault_injection_process();
         uds_process();
         watchdog_kick();
         k_sleep(K_MSEC(10));
