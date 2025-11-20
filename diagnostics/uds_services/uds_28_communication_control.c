9c98d8824df74e31d54f3b501e0444e80caf1aad
Raul Latorre <raul.latorre@gmail.com>
2025-11-20 21:34:54 +0100

    Create uds_28_communication_control.c

---
+++ diagnostics/uds_services/uds_28_communication_control.c
@@ -0,0 +1,7 @@
+#include "uds_28_communication_control.h"
+
+void uds_28_handle(uint8_t *req, uint16_t len, uint8_t *rsp, uint16_t *rsp_len)
+{
+    rsp[0] = 0x68; // Positive response
+    *rsp_len = 1;
+}
