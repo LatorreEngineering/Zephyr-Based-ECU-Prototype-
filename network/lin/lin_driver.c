f5b424ff83b2ade3457cbb9d83e6b48bb6e91d57
Raul Latorre <raul.latorre@gmail.com>
2025-11-20 21:14:03 +0100

    Update lin_driver.c

--- network/lin/lin_driver.c
+++ network/lin/lin_driver.c
@@ -1,14 +1,19 @@
 #include "lin_driver.h"
 #include <zephyr/drivers/uart.h>
 
-#define LIN_BAUD 19200
-
 static const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart3));
+
+int lin_init(void)
+{
+    if (!device_is_ready(uart)) return -1;
+    uart_configure(uart, 19200, UART_CFG_PARITY_NONE, UART_CFG_STOP_BITS_1);
+    return 0;
+}
 
 int lin_send_break(void)
 {
     uart_line_ctrl_set(uart, UART_LINE_CTRL_BREAK, 1);
-    k_sleep(K_USEC(800));   // break field
+    k_sleep(K_USEC(800));
     uart_line_ctrl_set(uart, UART_LINE_CTRL_BREAK, 0);
     return 0;
 }
@@ -16,7 +21,7 @@ int lin_send_break(void)
 int lin_send_header(uint8_t pid)
 {
     lin_send_break();
-    uart_poll_out(uart, 0x55);  // sync
-    uart_poll_out(uart, pid);   // PID
+    uart_poll_out(uart, 0x55); // Sync
+    uart_poll_out(uart, pid);
     return 0;
 }
