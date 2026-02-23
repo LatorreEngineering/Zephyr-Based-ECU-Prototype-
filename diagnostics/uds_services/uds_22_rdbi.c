77c6da9ad3d3ddb39e235a602abb3a640bba5611
Raul Latorre <raul.latorre@gmail.com>
2026-02-23 16:05:27 +0100

    Refactor uds_22_handle for improved response handling
    Updated response handling and added error checking for message length. Changed data identifiers to 16-bit values and adjusted response lengths accordingly.

--- diagnostics/uds_services/uds_22_rdbi.c
+++ diagnostics/uds_services/uds_22_rdbi.c
@@ -1,33 +1,49 @@
 #include "uds_22_rdbi.h"
-#include "../app/sensor_sim.h"
+#include "../../app/sensor_sim.h"
+#include <zephyr/kernel.h>
 
 void uds_22_handle(uint8_t *req, uint16_t len, uint8_t *rsp, uint16_t *rsp_len)
 {
-    if (len < 2) return;
-    uint8_t did = req[1];
+    if (len < 3) {
+        rsp[0] = 0x7F;
+        rsp[1] = 0x22;
+        rsp[2] = 0x13; // incorrectMessageLengthOrInvalidFormat
+        *rsp_len = 3;
+        return;
+    }
+    
+    uint16_t did = ((uint16_t)req[1] << 8) | req[2];
 
+    rsp[0] = 0x62; // Positive response for 0x22
+    rsp[1] = req[1]; // Echo DID high byte
+    rsp[2] = req[2]; // Echo DID low byte
+    
     switch(did) {
-        case 0x01: { // RPM
+        case 0x0001: { // RPM
             uint16_t rpm = sensor_get_rpm();
-            rsp[0] = (rpm >> 8) & 0xFF;
-            rsp[1] = rpm & 0xFF;
-            *rsp_len = 2;
+            rsp[3] = (rpm >> 8) & 0xFF;
+            rsp[4] = rpm & 0xFF;
+            *rsp_len = 5;
             break;
         }
-        case 0x02: { // Temperature
+        case 0x0002: { // Temperature
             float temp = sensor_get_temp();
-            rsp[0] = (uint8_t)temp;
-            *rsp_len = 1;
+            rsp[3] = (uint8_t)temp;
+            *rsp_len = 4;
             break;
         }
-        case 0x03: { // Voltage
+        case 0x0003: { // Voltage
             float volt = sensor_get_voltage();
-            rsp[0] = (uint8_t)(volt*10);
-            *rsp_len = 1;
+            rsp[3] = (uint8_t)(volt * 10);
+            *rsp_len = 4;
             break;
         }
         default:
-            *rsp_len = 0;
+            // Data identifier not supported
+            rsp[0] = 0x7F;
+            rsp[1] = 0x22;
+            rsp[2] = 0x31; // requestOutOfRange
+            *rsp_len = 3;
             break;
     }
 }
