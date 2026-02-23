38feee1ae98f010cd34af231351bcd33b3c6f626
Raul Latorre <raul.latorre@gmail.com>
2026-02-23 16:19:30 +0100

    Refactor uds_31_handle to include parameter casts
    Added void casts for request and length parameters.

--- diagnostics/uds_services/uds_31_routine_control.c
+++ diagnostics/uds_services/uds_31_routine_control.c
@@ -1,7 +1,11 @@
 #include "uds_31_routine_control.h"
+#include <zephyr/kernel.h>
 
 void uds_31_handle(uint8_t *req, uint16_t len, uint8_t *rsp, uint16_t *rsp_len)
 {
-    rsp[0] = 0x71;
+    (void)req;
+    (void)len;
+    
+    rsp[0] = 0x71; // Positive response for 0x31
     *rsp_len = 1;
 }
