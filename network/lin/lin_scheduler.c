ac9a9d8d87882b088961baf4e242a04bafa76be9
Raul Latorre <raul.latorre@gmail.com>
2026-02-23 09:24:13 +0100

    Refactor LIN scheduler to manage running state

--- network/lin/lin_scheduler.c
+++ network/lin/lin_scheduler.c
@@ -3,17 +3,29 @@
 #include <zephyr/kernel.h>
 
 static struct k_timer lin_timer;
+static bool scheduler_running = false;
 
 static void lin_timer_handler(struct k_timer *timer)
 {
+    (void)timer;
     static uint8_t pid = 0x10;
-    lin_send_header(pid);
-    pid++;
+    
+    if (scheduler_running) {
+        lin_send_header(pid);
+        pid++;
+        if (pid > 0x3F) {
+            pid = 0x10;
+        }
+    }
 }
 
 void lin_scheduler_start(void)
 {
-    lin_init();
-    k_timer_init(&lin_timer, lin_timer_handler, NULL);
-    k_timer_start(&lin_timer, K_MSEC(20), K_MSEC(20)); // 50Hz
+    if (!scheduler_running) {
+        lin_init();
+        k_timer_init(&lin_timer, lin_timer_handler, NULL);
+        k_timer_start(&lin_timer, K_MSEC(20), K_MSEC(20)); // 50Hz
+        scheduler_running = true;
+        printk("LIN scheduler started\n");
+    }
 }
