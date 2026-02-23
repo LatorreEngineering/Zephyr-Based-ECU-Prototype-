3f721edae217648ce0c984e4c67b266d0a92174a
Raul Latorre <raul.latorre@gmail.com>
2026-02-23 16:14:35 +0100

    Refactor uds_28_handle to include request handling

--- diagnostics/uds_services/uds_28_communication_control.c
+++ diagnostics/uds_services/uds_28_communication_control.c
@@ -1,7 +1,11 @@
 #include "uds_28_communication_control.h"
+#include <zephyr/kernel.h>
 
 void uds_28_handle(uint8_t *req, uint16_t len, uint8_t *rsp, uint16_t *rsp_len)
 {
-    rsp[0] = 0x68; // Positive response
+    (void)req;
+    (void)len;
+    
+    rsp[0] = 0x68; // Positive response for 0x28
     *rsp_len = 1;
 }
