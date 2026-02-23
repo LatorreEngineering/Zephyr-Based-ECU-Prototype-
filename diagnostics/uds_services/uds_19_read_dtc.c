b69def2201a90d99358de9b2343975470025db6d
Raul Latorre <raul.latorre@gmail.com>
2026-02-23 16:02:26 +0100

    Update uds_19_handle to process DTC requests
    Handle request for DTC status and respond accordingly.

--- diagnostics/uds_services/uds_19_read_dtc.c
+++ diagnostics/uds_services/uds_19_read_dtc.c
@@ -1,14 +1,32 @@
 #include "uds_19_read_dtc.h"
 #include "../dtc_manager.h"
+#include <zephyr/kernel.h>
 
 void uds_19_handle(uint8_t *req, uint16_t len, uint8_t *rsp, uint16_t *rsp_len)
 {
-    uint8_t idx = 0;
-    for (uint8_t i=0; i<256; i++) {
-        if (dtc_get_status(i)) {
-            rsp[idx++] = i;
-            if (idx >= *rsp_len) break;
-        }
+    if (len < 2) {
+        rsp[0] = 0x7F;
+        rsp[1] = 0x19;
+        rsp[2] = 0x13;
+        *rsp_len = 3;
+        return;
     }
-    *rsp_len = idx;
+    
+    uint8_t sub_function = req[1];
+    
+    rsp[0] = 0x59; // Positive response for 0x19
+    rsp[1] = sub_function;
+    *rsp_len = 2;
+    
+    // Report number of DTCs
+    if (sub_function == 0x01) { // reportNumberOfDTCByStatusMask
+        uint8_t count = 0;
+        for (int i = 0; i < 256; i++) {
+            if (dtc_get_status(i)) {
+                count++;
+            }
+        }
+        rsp[2] = count;
+        *rsp_len = 3;
+    }
 }
