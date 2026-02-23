089c79ad7faffb45754d2f607bbf8d2f5010b3e7
Raul Latorre <raul.latorre@gmail.com>
2026-02-23 16:23:18 +0100

    Add Zephyr kernel include and improve response handling

--- diagnostics/uds_services/uds_37_transfer_exit.c
+++ diagnostics/uds_services/uds_37_transfer_exit.c
@@ -1,7 +1,11 @@
 #include "uds_37_transfer_exit.h"
+#include <zephyr/kernel.h>
 
 void uds_37_handle(uint8_t *req, uint16_t len, uint8_t *rsp, uint16_t *rsp_len)
 {
-    rsp[0] = 0x77;
+    (void)req;
+    (void)len;
+    
+    rsp[0] = 0x77; // Positive response for 0x37
     *rsp_len = 1;
 }
