82ce6924de6f6e9fdd6ac41ba043a7edfac88edb
Raul Latorre <raul.latorre@gmail.com>
2026-02-23 15:54:41 +0100

    Implement ECU reset handling in uds_11_handle
    Handle different ECU reset types and respond accordingly.

--- diagnostics/uds_services/uds_11_ecu_reset.c
+++ diagnostics/uds_services/uds_11_ecu_reset.c
@@ -1,12 +1,36 @@
 #include "uds_11_ecu_reset.h"
+#include "../../app/ecu_state_machine.h"
 #include <zephyr/kernel.h>
+#include <zephyr/sys/reboot.h>
 
 void uds_11_handle(uint8_t *req, uint16_t len, uint8_t *rsp, uint16_t *rsp_len)
 {
-    rsp[0] = 0x51; // Positive response
-    *rsp_len = 1;
-
-    // Simulate ECU reset after response
+    if (len < 2) {
+        rsp[0] = 0x7F;
+        rsp[1] = 0x11;
+        rsp[2] = 0x13; // incorrectMessageLengthOrInvalidFormat
+        *rsp_len = 3;
+        return;
+    }
+    
+    uint8_t reset_type = req[1];
+    
+    rsp[0] = 0x51; // Positive response for 0x11
+    rsp[1] = reset_type;
+    *rsp_len = 2;
+    
+    // Trigger reset after sending response
     k_sleep(K_MSEC(100));
-    sys_reboot(SYS_REBOOT_COLD);
+    
+    switch (reset_type) {
+    case 0x01: // hardReset
+        sys_reboot(SYS_REBOOT_COLD);
+        break;
+    case 0x02: // keyOffOnReset
+    case 0x03: // softReset
+        ecu_trigger_reset();
+        break;
+    default:
+        break;
+    }
 }
