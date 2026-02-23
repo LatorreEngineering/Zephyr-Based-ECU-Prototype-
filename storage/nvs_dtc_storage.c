e027dba921799d3af9033259269422580905110d
Raul Latorre <raul.latorre@gmail.com>
2026-02-23 15:42:42 +0100

    Optimize NVS initialization and read/write operations
    Refactor NVS initialization and data handling for efficiency.

--- storage/nvs_dtc_storage.c
+++ storage/nvs_dtc_storage.c
@@ -1,28 +1,56 @@
 #include "nvs_dtc_storage.h"
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/fs/nvs.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <string.h>
 
 static struct nvs_fs fs;
 
+#define NVS_PARTITION_ID FIXED_PARTITION_ID(storage_partition)
+#define NVS_PARTITION_DEVICE FIXED_PARTITION_DEVICE(storage_partition)
+
 void nvs_init(void)
 {
-    fs.offset = FLASH_AREA_OFFSET(storage);
-    fs.sector_size = 4096;
-    fs.sector_count = 4;
-    fs.flash_device = DEVICE_DT_GET(DT_CHOSEN(zephyr_flash_controller));
-    nvs_init(&fs, NULL);
+    struct flash_pages_info info;
+    int rc;
+
+    fs.flash_device = NVS_PARTITION_DEVICE;
+    if (!device_is_ready(fs.flash_device)) {
+        printk("Flash device not ready\n");
+        return;
+    }
+
+    fs.offset = FIXED_PARTITION_OFFSET(storage_partition);
+    
+    rc = flash_get_page_info_by_offs(fs.flash_device, fs.offset, &info);
+    if (rc) {
+        printk("Unable to get page info: %d\n", rc);
+        return;
+    }
+    
+    fs.sector_size = info.size;
+    fs.sector_count = 4U;
+
+    rc = nvs_mount(&fs);
+    if (rc) {
+        printk("Flash Init failed: %d\n", rc);
+    } else {
+        printk("NVS initialized successfully\n");
+    }
 }
 
 void nvs_save_dtc(bool *dtc_table, size_t len)
 {
-    for (size_t i=0; i<len; i++) {
-        nvs_write(&fs, i, &dtc_table[i], sizeof(bool));
-    }
+    // Save in chunks to avoid too many writes
+    nvs_write(&fs, 1, dtc_table, len);
 }
 
 void nvs_load_dtc(bool *dtc_table, size_t len)
 {
-    for (size_t i=0; i<len; i++) {
-        nvs_read(&fs, i, &dtc_table[i], sizeof(bool));
+    int rc = nvs_read(&fs, 1, dtc_table, len);
+    if (rc < 0) {
+        // Initialize to false if read fails
+        memset(dtc_table, 0, len);
     }
 }
