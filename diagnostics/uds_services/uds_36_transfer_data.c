c01a45e0ca1735c1b65aba5aa0d3cd0e32aba1fd
Raul Latorre <raul.latorre@gmail.com>
2026-02-23 16:22:20 +0100

    Handle unused parameters in uds_36_handle function
    Added unused variable casts for request and length parameters.

--- diagnostics/uds_services/uds_36_transfer_data.c
+++ diagnostics/uds_services/uds_36_transfer_data.c
@@ -1,7 +1,11 @@
 #include "uds_36_transfer_data.h"
+#include <zephyr/kernel.h>
 
 void uds_36_handle(uint8_t *req, uint16_t len, uint8_t *rsp, uint16_t *rsp_len)
 {
-    rsp[0] = 0x76;
+    (void)req;
+    (void)len;
+    
+    rsp[0] = 0x76; // Positive response for 0x36
     *rsp_len = 1;
 }
