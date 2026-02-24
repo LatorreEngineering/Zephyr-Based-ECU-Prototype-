f6603c91ca75782f27b596dad1429b75eb486471
Raul Latorre <raul.latorre@gmail.com>
2026-02-24 14:20:42 +0100

    Update can_transport.h

--- network/can/can_transport.h
+++ network/can/can_transport.h
@@ -2,5 +2,5 @@
 #include <stdint.h>
 
 void can_init(void);
-int can_send(uint8_t *data, uint8_t len);
-int can_receive(uint8_t *buf, uint8_t max_len);
+int ecu_can_send(uint8_t *data, uint8_t len);
+int ecu_can_receive(uint8_t *buf, uint8_t max_len);
